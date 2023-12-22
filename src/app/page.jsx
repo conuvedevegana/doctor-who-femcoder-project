a3b741f74e9c6141b854c2b2d253b8d8382b176c
conuvedevegana <cardelsil@gmail.com>
2023-12-22 19:50:50 +0100

    deleted old code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
-import MyButton from './components/button'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>DOCTOR WHO</h1>
-      <MyButton/>
     </main>
   )
 }
