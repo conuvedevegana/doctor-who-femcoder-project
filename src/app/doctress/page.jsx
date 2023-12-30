d544175f4eba141a3c7b21e7221d46a12a41c6ac
conuvedevegana <cardelsil@gmail.com>
2023-12-30 20:54:39 +0100

    created button

---
+++ src/app/doctress/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Button from '../components/Button/button'
+
+export default function pageDoctress() {
+  return (
+    <>
+    <Header/>
+    <Button/>
+    </>
+  )
+}
